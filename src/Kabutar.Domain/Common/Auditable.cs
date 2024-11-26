c8a9c5a5d8a93d58b4c914f51b57237b65d1b932
Ulugbek Keldibekov <keldibekovulugbek@gmail.com>
2024-11-26 22:10:28 +0500

    Project architectured

---
+++ src/Kabutar.Domain/Common/Auditable.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Kabutar.Domain.Common;
+
+public class Auditable : BaseEntity
+{
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public DateTime Updated { get; set; }
+}
