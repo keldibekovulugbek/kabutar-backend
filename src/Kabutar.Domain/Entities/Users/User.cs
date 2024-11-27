850d6e7ac645e86e5f9063eaf9c458a4a9eeb8d5
Ulugbek Keldibekov <keldibekovulugbek@gmail.com>
2024-11-27 09:09:27 +0500

    Models created

---
+++ src/Kabutar.Domain/Entities/Users/User.cs
@@ -0,0 +1,24 @@
+﻿using Kabutar.Domain.Common;
+
+namespace Kabutar.Domain.Entities.Users;
+
+public class User : Auditable
+{
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+    
+    public string Username { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+    
+    public bool IsEmailVerified { get; set; } = false;
+
+    public string PasswordHash { get; set; } = string.Empty;
+
+    public string PasswordSalt { get; set; } = string.Empty;
+
+    public string About { get; set; } = string.Empty;
+
+    public DateTime? LastActive { get; set; }
+}
