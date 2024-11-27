ca9d4d0505b6a60b821383d5f8083cb303b48316
Ulugbek Keldibekov <keldibekovulugbek@gmail.com>
2024-11-27 10:35:56 +0500

    added repositories

---
+++ src/Kabutar.DataAccess/Interfaces/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using Kabutar.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Kabutar.DataAccess.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(long id);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
--- src/Kabutar.Domain/Entities/Messages/Message.cs
+++ src/Kabutar.Domain/Entities/Messages/Message.cs
@@ -3,22 +3,21 @@
 using Kabutar.Domain.Common;
 using Kabutar.Domain.Entities.Users;
 
-namespace Kabutar.Domain.Entities.Messages 
+namespace Kabutar.Domain.Entities.Messages; 
+
+public class Message : Auditable
 {
-    public class Message : Auditable
-    {
-        public string Content { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
 
-        public long SenderId { get; set; }
-        public User Sender { get; set; } = null!;
+    public long SenderId { get; set; }
+    public User Sender { get; set; } = null!;
 
-        public long ReceiverId { get; set; }
-        public User Receiver { get; set; } = null!;
+    public long ReceiverId { get; set; }
+    public User Receiver { get; set; } = null!;
 
-        public bool IsRead { get; set; } = false;
-        
-        public bool IsDeletedBySender { get; set; } = false;
-        
-        public bool IsDeletedByReceiver { get; set; } = false;
-    }
+    public bool IsRead { get; set; } = false;
+    
+    public bool IsDeletedBySender { get; set; } = false;
+    
+    public bool IsDeletedByReceiver { get; set; } = false;
 }
