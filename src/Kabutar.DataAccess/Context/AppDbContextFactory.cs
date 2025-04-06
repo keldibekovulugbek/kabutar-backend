917150ea46afd847a1f8e3107ecc1016835e70e5
Ulugbek Keldibekov <keldibekovulugbek@gmail.com>
2025-04-06 21:33:37 +0500

    add db factory

---
+++ src/Kabutar.DataAccess/Context/AppDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Kabutar.DataAccess.Context
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            // 📍 Bu yerni loyihangga moslashtir
+            var basePath = Path.Combine(Directory.GetCurrentDirectory(), "../Kabutar.Api");
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
